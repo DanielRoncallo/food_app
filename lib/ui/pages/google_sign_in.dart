061b21676d0c7f4f6fdabbe26abea36b7a7f3984
DanielRoncallo <roncallodaniel@hotmail.com>
2022-03-17 14:57:18 -0500

    fix

--- lib/ui/pages/google_sign_in.dart
+++ lib/ui/pages/google_sign_in.dart
@@ -1,35 +1,51 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_app/ui/pages/login_controller.dart';
+import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInButton extends StatefulWidget {
-   @override
+  @override
   _GoogleSignInButtonInState createState() => _GoogleSignInButtonInState();
 }
 
 class _GoogleSignInButtonInState extends State<GoogleSignInButton> {
+  final controller = Get.put(LoginController());
   @override
   Widget build(BuildContext context) {
     // we return the MaterialApp here ,
     // MaterialApp contain some basic ui for android ,
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('gooogle'),
-      ),
-      body: Center(
-        child: FloatingActionButton.extended(
-            onPressed: () {
-               GoogleSignIn().signIn();
-            },
-            icon: Image.asset(
-              'images/google.png',
-              height: 32,
-              width: 32,
-            ),
-            label: Text('sign google'),
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black),
-      ),
+    //cambiar el sign in en la carpeta apropiada
+    return Center(child: Obx(() {
+      if (controller.googleAccount.value == null) {
+        return buildLogin();
+      }else{
+        return buildProfileView();
+      }
+  
+    }));
+  }
+
+  Column buildProfileView() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [CircleAvatar(), Text('email'), Text('name')],
     );
+    
+  }
+
+  FloatingActionButton buildLogin() {
+    return FloatingActionButton.extended(
+        onPressed: () {
+          controller.login();
+        },
+        icon: Image.asset(
+          'images/google.png',
+          height: 32,
+          width: 32,
+        ),
+        label: Text('sign google'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black);
   }
 }
